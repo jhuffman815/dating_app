0aab07513aa0816829b2b8cdf346bdad96d8dc18
Jewell <jhuffman815@gmail.com>
2020-05-22 00:29:54 -0400

    Firebase DB

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 const firebaseConfig = {
     apiKey: "AIzaSyATw-u1lKKh38osHOWf9XMc70kIeTpH3T8",
@@ -17,6 +18,7 @@ const firebaseConfig = {
           app.initializeApp(firebaseConfig);
 
           this.auth = app.auth();
+          this.db = app.database();
       }
 
       //*** Auth API ***/
@@ -26,6 +28,12 @@ const firebaseConfig = {
       doSignOut = () => this.auth.signOut();
       doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
       doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+      //*** User API ***/
+
+      user = uid => this.db.ref(`users/${uid}`);
+
+      users = () => this.db.ref('users');
   }
 
   export default Firebase;
